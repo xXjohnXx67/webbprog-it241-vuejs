a157dc3e6495015a1b7b37f2e7908e31a57e294a
John Adrian Ochoa <jcochoa@student.apc.edu.ph>
2026-01-06 09:33:11 +0800

    added vue images

---
+++ home/js/images.js
@@ -0,0 +1,18 @@
+  const app = Vue.createApp({
+   data() {
+    return {
+     manyfoods: [
+        'https://www.w3schools.com/vue/img_burrito.svg',
+        'https://www.w3schools.com/vue/img_salad.svg',
+        'https://www.w3schools.com/vue/img_cake.svg', 
+        'https://www.w3schools.com/vue/img_soup.svg',
+        'https://www.w3schools.com/vue/img_fish.svg',
+        'https://www.w3schools.com/vue/img_pizza.svg',
+        'https://www.w3schools.com/vue/img_rice.svg'
+
+     ]
+    }
+   }
+  })
+  
+  app2.mount('#app2')
