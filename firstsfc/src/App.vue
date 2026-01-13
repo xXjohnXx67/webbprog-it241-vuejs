c878785267671862acc510021d12d8bef4069733
John Adrian Ochoa <jcochoa@student.apc.edu.ph>
2026-01-13 00:50:36 +0000

    added food items

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,18 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
-<style scoped></style>
+<script></script>
+
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
