093030b6362a9a9fc7b3534e7e707b0937dca184
John Adrian Ochoa <jcochoa@student.apc.edu.ph>
2026-01-19 23:13:22 +0800

    Fix

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
-import FoodItem from './components/FoodItem2.vue'
+import FoodItem2 from './components/FoodItem2.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
